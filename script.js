956e28d272993d8351624705ef0af55fdd558579
JoeTheorium <fernandoandresraggio@gmail.com>
2023-05-11 21:47:09 -0300

    Restart with maze but more easy xD

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-function rand(max) {
-    return Math.floor(Math.random() * max);
+let levels = [];
+
+levels [0] = {
+    map: [
+        [1, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0],
+        [0, 0, 0, 1, 0],
+        [0, 1, 0, 1, 0]
+    ],
+    player: {
+        x: 0,
+        y: 4
+    },
+    goal: {
+        x: 4,
+        y: 1
+    },
+    theme: 'default'
+};
+
+function Game(id, level) {
+    
 }
 
